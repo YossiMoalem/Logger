f66d991e5b4980eb18d7507864f3c19e2a256a68
YossiMoalem <yosim@amdocs.com>
2014-06-05 11:41:40 +0300

    New, easy to use, macros to use the logger

---
+++ smartLogger.h
@@ -0,0 +1,31 @@
+#ifndef SMART_LOGGER_H
+#define SMART_LOGGER_H
+
+#include "logMngr.h"
+
+/******************************************************************************\
+ * SmartLogger:
+ * This provides a set of macros to easily use the logger:
+ * First, obtain a logger handle:
+ *     logMngr* pLogger = GET_LOGGER(90, pLogWriter); 
+ * Now you can log messages:
+ *      LOG_MSG(pLogger, "My Text", 20);
+ *Finally, wen the  application terminates, shutdown the logger:
+      SHUTDOWN_LOGGER(pLogger);
+ *
+ ******************************************************************************/
+
+#define GET_LOGGER(Severity, pWritter) \
+         new logMngr(Severity, pWritter);
+
+#define LOG_MSG(pLogger, Text, Severity) \
+               pLogger->write(Text, __PRETTY_FUNCTION__, time(0), pthread_self(), Severity, false);  
+
+#define LOG_MSG_WITH_STACK (pLogger, Text, Severity) \
+               pLogger->write(Text, __PRETTY_FUNCTION__, time(0), pthread_self(), Severity, true);  
+
+#define SHUTDOWN_LOGGER(pLogger) \
+               pLogger->shutDown();
+
+#endif//SMART_LOGGER_H
+
